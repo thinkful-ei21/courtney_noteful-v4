1ebd080ef523250bfe3da1b5762c019b508ad0c7
Courtney Adams <cadams8238@gmail.com>
2018-06-11 13:12:50 -0700

    added bcryptjs

--- routes/users.js
+++ routes/users.js
@@ -11,16 +11,24 @@ const User = require('../models/user');
 router.post('/', (req, res, next) => {
 	const {username, password, fullname} = req.body;
 
-	return User.create({
-		username,
-		password,
-		fullname
-	})
-	.then(user => {
-		res.status(201).location(`/api/users/${user.id}`).json(user);
-	})
-	.catch(err => next(err));
-
+	return User.hashPassword(password)
+		.then(digest => {
+			return User.create({
+				username,
+				password: digest,
+				fullname
+			});
+		})
+		.then(user => {
+			res.status(201).location(`/api/users/${user.id}`).json(user);
+		})
+		.catch(err => {
+			if (err.code === 11000) {
+	      err = new Error('The username already exists');
+	      err.status = 400;
+    	}
+			next(err);
+		});
 });
 
 
