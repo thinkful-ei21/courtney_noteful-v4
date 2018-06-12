7947a7703e150dff2530518d42555c88dfff5108
Courtney Adams <cadams8238@gmail.com>
2018-06-12 12:29:47 -0700

    added JWT to routes and updated server.js

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
 const localStrategy = require('./passport/local');
+const jwtStrategy = require('./passport/jwt');
 
 const { PORT, MONGODB_URI } = require('./config');
 
@@ -17,6 +18,7 @@ const authRouter = require('./routes/auth');
 // Create an Express application
 const app = express();
 passport.use(localStrategy);
+passport.use(jwtStrategy);
 
 // Log all requests. Skip logging during
 app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'common', {
