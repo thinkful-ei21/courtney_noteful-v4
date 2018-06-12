5c88cc9eba12b254a7f5b015545012d4866d21c7
Courtney Adams <cadams8238@gmail.com>
2018-06-12 13:12:36 -0700

    added refresh endpoint to auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -10,6 +10,7 @@ const {JWT_SECRET, JWT_EXPIRY} = require('../config');
 const options = {session: false, failWithError: true};
 const localAuth = passport.authenticate('local', options);
 
+const jwtAuth = passport.authenticate('jwt', {session: false, failWithError: true});
 
 function createAuthToken(user) {
 	return jwt.sign({user}, JWT_SECRET, {
@@ -22,6 +23,11 @@ function createAuthToken(user) {
 router.post('/login', localAuth, function (req, res) {
   const authToken = createAuthToken(req.user);
   res.json({authToken});
+});
+
+router.post('/refresh', jwtAuth, (req, res) => {
+	const authToken = createAuthToken(req.user);
+	res.json({authToken});
 });
 
 module.exports = router;
