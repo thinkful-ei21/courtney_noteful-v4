31ceaa820d48330e867dd668b01d56cc962c17d9
Courtney Adams <cadams8238@gmail.com>
2018-06-15 15:10:35 -0700

    updated mongodb in .config

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 8080,
-  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost/noteful',
+  MONGODB_URI: process.env.MONGODB_URI || 'mongodb://user:password1@ds261450.mlab.com:61450/heroku_7sxtsmh0',
   TEST_MONGODB_URI: process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test',
   JWT_SECRET: process.env.JWT_SECRET,
   JWT_EXPIRY: process.env.JWT_EXPIRY || '7d'
